6121cb35d9f769863121bd3e620e9dd2bb44106d
MandichRiccardo <ricky.mandich@gmail.com>
2023-12-13 23:03:18 +0100

    creazione 2023 12 13 23:00

---
+++ helloWorld.java
@@ -0,0 +1,12 @@
+public class helloWorld
+{
+    public static void output(String a){
+        System.out.println(a);
+    }
+    
+    public static void main(String args[]){
+        int x = 5;
+        int y = 63;
+        output("Hello World\nil tuo risultato è:\t" + x * y);
+    }
+}
